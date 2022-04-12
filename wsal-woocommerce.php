ebb3d26398aea6c45813d0e0958920f20a4abf5d
Martin Krcho <martin@wpwhitesecurity.com>
2022-04-12 13:31:25 +0200

    Change 9135 event description and bump version number. (#172)

--- wsal-woocommerce.php
+++ wsal-woocommerce.php
@@ -6,7 +6,7 @@
  * Text Domain: wsal-woocommerce
  * Author: WP White Security
  * Author URI: http://www.wpwhitesecurity.com/
- * Version: 1.4.1
+ * Version: 1.4.2
  * License: GPL2
  * Network: true
  *
